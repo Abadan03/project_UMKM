aab8ee5036b8f48f6f888eda81a7b9c10cc05ce5
Bob <dinarabadan03@gmail.com>
2026-06-18 23:58:47 +0700

    asdasdasda

--- resources/js/Pages/MainLayout.tsx
+++ resources/js/Pages/MainLayout.tsx
@@ -11,7 +11,7 @@ export default function MainLayout({
             {/* Navbar ditaruh di sini sekali saja */}
             <NavbarLayout />
 
-            {/* 'children' adalah tempat masuknya konten dari Home atau Feature */}
+            {/* 'children' adalah tempat masuknya konten dari Home atau Feature kontolodon */}
             <main className="mt-8">{children}</main>
         </div>
     );
