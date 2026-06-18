da29a582928f967361dab183124a318ce337c9a5
Rafif Rais <rafifrais12@gmail.com>
2026-06-19 00:08:13 +0700

    test

--- resources/js/Pages/components/zero.tsx
+++ resources/js/Pages/components/zero.tsx
@@ -1,23 +1,21 @@
 import { Button } from "@/components/ui/button";
 import zeroSC from "@/asset/img/zeroSC.png";
-
 import { Sparkles } from "lucide-react";
-import React from "react";
 
 export default function Section1() {
     return (
         <>
-            <section className="min-h-screen flex flex-col items-center justify-center p-4 pt-16">
+            <section className="min-h-screen flex flex-col items-center justify-center pt-16">
                 <h1 className="text-5xl font-bold leading-tight text-center text-slate-900">
                     The Ultimate Solution For
                     <br />
                     Optimizing Your Sales Process
                 </h1>
 
-                <div className="mt-16 grid gap-12 lg:grid-cols-2 lg:items-center">
+                <div className="mt-12 grid gap-12 lg:grid-cols-2 lg:items-center">
                     {/* Dashboard Image */}
                     <div>
-                        <img src={zeroSC} alt="Dashboard" className="w-full " />
+                        <img src={zeroSC} alt="Dashboard" />
                     </div>
 
                     {/* Content */}
@@ -29,49 +27,6 @@ export default function Section1() {
                             Platform Empowers You To Take Control Of Your Sales
                             Processes With Ease.
                         </p>
-
-                        <div className="mt-8 flex gap-4">
-                            <Button className="bg-teal-700 hover:bg-teal-800">
-                                Get Started
-                            </Button>
-
-                            <Button
-                                variant={"outline"}
-                                className="border-slate-300"
-                            >
-                                Learn More
-                            </Button>
-                        </div>
-
-                        <div className="mt-10 grid grid-cols-3 gap-6">
-                            <div>
-                                <h3 className="text-4xl font-bold text-slate-900">
-                                    50M
-                                </h3>
-                                <p className="mt-1 text-sm text-slate-500">
-                                    Downloaded
-                                </p>
-                            </div>
-
-                            <div>
-                                <h3 className="text-4xl font-bold text-slate-900">
-                                    190+
-                                </h3>
-                                <p className="mt-1 text-sm text-slate-500">
-                                    Active user
-                                </p>
-                            </div>
-
-                            <div>
-                                <h3 className="text-4xl font-bold text-slate-900">
-                                    100+
-                                </h3>
-                                <p className="mt-1 text-sm text-slate-500">
-                                    Business teams
-                                </p>
-                            </div>
-                        </div>
-
                         <div className="mt-10 flex items-center gap-4 rounded-xl bg-slate-50 p-4">
                             <Sparkles />
 
---
+++ resources/js/Pages/index.js
@@ -0,0 +1,1 @@
+console.log("INI KODE SAYA");
